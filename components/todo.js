0f19621e1728a525990e10d25227432f09790a8d
alinaasrzad <95015661+alinaasrzad@users.noreply.github.com>
2024-03-25 19:58:45 +0330

    fized the styles

--- components/todo.js
+++ components/todo.js
@@ -53,3 +53,11 @@ function newElement() {
 
 // }}
 // )
+// document.getElementById("delete").addEventListener("click", function(e) {
+// 	// e.target is the clicked element!
+// 	// If it was a list item
+// 	if(e.target && e.target.nodeName == "LI") {
+// 		// List item found!  Output the ID!
+// 		console.log("List item ", e.target.id.replace("post-", ""), " was clicked!");
+// 	}
+// });
